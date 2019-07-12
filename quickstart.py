cee48df2c8ee7830423d50dea1a8de467ec3b5a4
ElDieguii <49466415+ElDieguii@users.noreply.github.com>
2019-07-12 01:29:07 -0300

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -35,15 +35,16 @@ def main():
 
     # Call the Drive v3 API
     results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+        pageSize=5, fields="nextPageToken, files(name, createdTime)").execute()
     items = results.get('files', [])
-
+    
     if not items:
         print('No files found.')
     else:
         print('Files:')
         for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
-
+            print(u'file:{0}, created time:{1}'.format(item['name'], item['createdTime']))
+            
+    
 if __name__ == '__main__':
     main()
